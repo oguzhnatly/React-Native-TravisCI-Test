d7f382d788e0d7562f3027441839e257a78cf4c7
Oğuzhan Atalay <oguzhnatly@gmail.com>
2021-02-11 20:55:59 +0300

    App.test.js writed.

--- App.js
+++ App.js
@@ -1,21 +1,89 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {SafeAreaView, View, Text, FlatList, StyleSheet, TouchableOpacity, TextInput} from 'react-native';
 
-export default function App() {
+/**
+ * TextInput: testID="input" (component which is user types the todo text)
+ * TouchableOpacity: testID="button" (component which is saves the todo to list)
+ * FlatList: testID="list" (list of todo)
+ */
+
+function App() {
+  const [toDoList, settoDoList] = useState([]);
+  const [counter, setCounter] = useState(toDoList.length);
+  const [todoItem, setTodoItem] = useState('');
+  const rendertoDo = ({item}) => (
+    <TouchableOpacity>{item.title}</TouchableOpacity>
+  );
+
+  useEffect(() => {
+    settoDoList(toDoList);
+  }, []);
+
+  function addToCart(title) {
+    if (!title) {
+      return;
+    }
+    settoDoList([
+      ...toDoList,
+      {id: toDoList.length, title: title, isDone: false},
+    ]);
+  }
+  useEffect(() => {
+    const counter = toDoList.filter((item) => item.isDone !== true);
+    setCounter(counter.length);
+  }, [toDoList]);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.container}>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            margin: 10,
+          }}>
+          <Text style={{fontSize: 30, fontWeight: 'bold', color: '#ff9e80'}}>
+            TODO
+          </Text>
+          <Text style={{fontSize: 30, fontWeight: 'bold', color: '#ff9e80'}}>
+            {counter}
+          </Text>
+        </View>
+        <FlatList
+          testID="list"
+          ListEmptyComponent={
+            <Text style={styles.emptyText}>
+              There is nothing to do, you can lie down..
+            </Text>
+          }
+          keyExtractor={(item, index) => index.toString()}
+          data={toDoList}
+          renderItem={rendertoDo}
+        />
+        
+        <View>
+          <TextInput
+            testID="input"
+            placeholder= "What are we doing today?"
+            placeholderTextColor="#f5f5f5"
+            onChangeText={(todo) => setTodoItem(todo)}></TextInput>
+          <TouchableOpacity testID="button" onPress={() => addToCart(todoItem)}>Ekle</TouchableOpacity>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#eceff1',
+  },
+  emptyText: {
+    alignSelf: 'center',
+    color: 'gray',
+    fontSize: 20,
   },
 });
+
+export default App;
--- App.test.js
+++ App.test.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import "react-native";
 import App from './App';
 
-describe('<App />', () => {
-  it('has 1 child', () => {
-    const tree = renderer.create(<App />).toJSON();
-    expect(tree.children.length).toBe(1);
-  });
+import { render, fireEvent } from '@testing-library/react-native';
+
+it("Renders coorectly", () => {
+  renderer.create(<App />);
+});
+
+it('Todo should be able to add to list', () => {
+  const wrapper = render(<App />);
+  const input_element = wrapper.getByTestId('input');
+  const button_element = wrapper.getByTestId('button');
+
+  fireEvent.changeText(input_element, 'TEST TODO');
+  fireEvent.press(button_element);
+
+  const list = wrapper.getByTestId('list');
+  expect(list.props.data.length).toBe(1);
 });
