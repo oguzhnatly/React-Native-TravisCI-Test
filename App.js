35a527e377820339b94cd29704960ec378190838
Oğuzhan Atalay <oguzhnatly@gmail.com>
2021-02-11 23:41:03 +0300

    Error-Display Example Test is written.

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
 // import App from "./examples/todo-list/App";
-import App from "./examples/todo-list/App";
+import App from "./examples/error-display/App";
 
 export default App;
